66d92697d7fb268e1add0d94aa030644be825ebb
franck gaspoz <franck.gaspoz@gmail.com>
2026-03-29 03:51:12 +0200

    fix speak pre processors

--- src/TTS/speak-pre-processors/line-end-italic-remover.js
+++ src/TTS/speak-pre-processors/line-end-italic-remover.js
@@ -1,4 +1,4 @@
-export default class ItalicRemover {
+export default class LineEndItalicRemover {
 
     constructor(ctx) {
         this.ctx = ctx
@@ -14,6 +14,7 @@ export default class ItalicRemover {
         const ap_r = ')'
         const eol = '\n'
         var r = ''
+        var rmv = ''
         while (i < text.length) {
             const pc = i > 0 ? text[i - 1] : null
             const c = text[i]
@@ -22,17 +23,23 @@ export default class ItalicRemover {
                 && pc != sym && pc != cite && pc != ap_r && pc != ap_l
                 && nc != sym && nc != cite && nc != ap_r && nc != ap_l
             ) {
-
                 if (x0 == -1) {
 
                     x0 = i
+                    rmv += c
                 }
                 else {
                     //console.log(text.substring(x0, i + 1))
                     x0 = -1
+                    rmv += c
+                    if (nc != eol) {
+                        r += rmv
+                        rmv = ''
+                    }
                 }
             } else {
                 if (x0 == -1) r += c
+                else rmv += c
             }
             i++
         }
